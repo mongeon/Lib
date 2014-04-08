e274873cca99ba502e05a1202520026641f2bced
GabrielM <gabrielmongeon@gmail.com>
2014-04-08 13:46:51 -0400

    Add files to GitHub

---
+++ Mongeon.Lib.Tests/StringTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mongeon.Lib.Strings;
+
+namespace Mongeon.Lib.Tests
+{
+    [TestClass]
+    public class StringTests
+    {
+        [TestMethod]
+        public void PadCenterWithoutCharacterTest()
+        {
+            string testString = "Hello";
+            Assert.AreEqual(testString.PadCenter(15, ' '), "     Hello     ");
+        }
+        [TestMethod]
+        public void PadCenterWithCharacterTest()
+        {
+            string testString = "Hello";
+            Assert.AreEqual(testString.PadCenter(15), "     Hello     ");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void PadCenterTooLongTest()
+        {
+            string testString = "Hello";
+            testString.PadCenter(3);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PadCenterWithNullStringTest()
+        {
+            string testString = null;
+            testString.PadCenter(15);
+        }
+
+
+
+    }
+}
---
+++ Mongeon.Lib/Strings/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Mongeon.Lib.Strings
+{
+    public static class Extensions
+    {
+        /// <summary>
+        /// Pad the string with space and will be centered.
+        /// </summary>
+        /// <param name="s">String to be centered</param>
+        /// <param name="totalWidth">Total length of the resulting string, including space</param>
+        /// <returns>The padded string</returns>
+        public static string PadCenter(this string s, int totalWidth)
+        {
+            return PadCenter(s, totalWidth, ' ');
+        }
+
+        /// <summary>
+        /// Pad the string with a character and will be centered.
+        /// </summary>
+        /// <param name="s">String to be centered</param>
+        /// <param name="totalWidth">Total length of the resulting string, including padding</param>
+        /// <param name="paddingCharacter">The character used to pad the string</param>
+        /// <returns>The padded string</returns>
+        public static string PadCenter(this string s, int totalWidth, char paddingCharacter)
+        {
+            if (s == null)
+            {
+                throw new ArgumentNullException("s");
+            }
+            if (s.Length > totalWidth)
+            {
+                throw new InvalidOperationException();
+            }
+            int halfSpace = (totalWidth - s.Length) / 2;
+            return s.PadRight(halfSpace + s.Length, paddingCharacter).PadLeft(totalWidth, paddingCharacter);
+        }
+    }
+}
